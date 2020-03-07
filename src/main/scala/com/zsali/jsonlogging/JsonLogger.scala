e76eccfffae8182410aacb487ba91b93ca32be9f
Zeshan S. Ali <zeshansali@gmail.com>
2020-03-07 12:20:27 -0600

    dev: add functions for all log levels
    1. Add functions for all log levels
    2. Only log if the selected level is enabled

--- src/main/scala/com/zsali/jsonlogging/JsonLogger.scala
+++ src/main/scala/com/zsali/jsonlogging/JsonLogger.scala
@@ -1,36 +1,40 @@
 package com.zsali.jsonlogging
 
+import net.logstash.logback.argument.StructuredArguments.entries
+import org.slf4j.LoggerFactory
+
+import scala.jdk.CollectionConverters._
+
 case class JsonLogger(name: String = "main") {
-  import net.logstash.logback.argument.StructuredArguments.entries
-  import org.slf4j.LoggerFactory
-
-  import scala.jdk.CollectionConverters._
-
   val logger = LoggerFactory.getLogger(name)
 
-  def info(message: String, context: Map[String, String] = Map(), maybeException: Option[Exception] = None): Unit = {
-    logger.info(message, entries(context.asJava), maybeException.getOrElse(None))
+  def trace(message: String, context: Map[String, String] = Map(), maybeException: Option[Exception] = None): Unit = {
+    if (logger.isTraceEnabled) {
+      logger.trace(message, entries(context.asJava), maybeException.getOrElse(None))
+    }
   }
-}
 
-object MyApp extends App {
-  val logger = JsonLogger("jsonConsoleAppender")
+  def debug(message: String, context: Map[String, String] = Map(), maybeException: Option[Exception] = None): Unit = {
+    if (logger.isDebugEnabled) {
+      logger.debug(message, entries(context.asJava), maybeException.getOrElse(None))
+    }
+  }
 
-  logger.info(
-    message = "This is an informational log message."
-  )
+  def info(message: String, context: Map[String, String] = Map(), maybeException: Option[Exception] = None): Unit = {
+    if (logger.isInfoEnabled) {
+      logger.info(message, entries(context.asJava), maybeException.getOrElse(None))
+    }
+  }
 
-  logger.info(
-    message = "This is an informational log message.",
-    context = Map(
-      "key1" -> "value1",
-      "key2" -> "value2"
-    )
-  )
+  def warn(message: String, context: Map[String, String] = Map(), maybeException: Option[Exception] = None): Unit = {
+    if (logger.isWarnEnabled) {
+      logger.warn(message, entries(context.asJava), maybeException.getOrElse(None))
+    }
+  }
 
-  logger.info(
-    message = "This is an informational log message.",
-    context = Map("key1" -> "value1", "key2" -> "value2"),
-    maybeException = Some(new RuntimeException("This is an exception"))
-  )
+  def error(message: String, context: Map[String, String] = Map(), maybeException: Option[Exception] = None): Unit = {
+    if (logger.isErrorEnabled) {
+      logger.error(message, entries(context.asJava), maybeException.getOrElse(None))
+    }
+  }
 }
